26b6543ab1ee8e24fb9a4678918862a5a01cc86f
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-22 16:08:28 +0100

    Add export function
    When the dll is loaded, it will return a function which will alter the global functions `os.listall`, `os.listfiles` and `os.listdirs` with updated versions from this dll; now also accepting fixed paths starting with a drive letter.

--- ml_fixes/ml_fixes.cpp
+++ ml_fixes/ml_fixes.cpp
@@ -2,5 +2,26 @@
 //
 
 #include "stdafx.h"
+#include "explorer.h"
+#include "lua.hpp"
 
+#define DLLEXPORT __declspec(dllexport)
 
+extern "C" DLLEXPORT int add_ml_fixes(lua_State* L) {
+	lua_getglobal(L, "os");
+
+	if (lua_isnil(L, -1))
+		return 0;
+
+	lua_pushstring(L, "listall");
+	lua_pushcfunction(L, listDirectory);
+	lua_rawset(L, -3);
+	lua_pushstring(L, "listfiles");
+	lua_pushcfunction(L, listDirectoryFiles);
+	lua_rawset(L, -3);
+	lua_pushstring(L, "listdirs");
+	lua_pushcfunction(L, listDirectoryDirs);
+	lua_rawset(L, -3);
+
+	return 0;
+}
